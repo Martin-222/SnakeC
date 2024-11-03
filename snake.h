1f6c3c3db86db517883d11db7d81a69025b47802
Martin-222 <185973025+Martin-222@users.noreply.github.com>
2024-11-04 00:00:00 +0500

    'add file snake.h'

---
+++ snake.h
@@ -0,0 +1,9 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+void initialize_snake(char board[HEIGHT][WIDTH]);
+void draw_board(char board[HEIGHT][WIDTH]);
+int update_game(char board[HEIGHT][WIDTH]);
+
+#endif // SNAKE_H
+
